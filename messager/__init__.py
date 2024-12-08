bab6ea827ede655077cdb2a6f9c0b055afcfcd80
eniocsjunior <eniocsjunior@gmail.com>
2024-12-08 16:52:35 -0300

    Add data in __init__

--- messager/__init__.py
+++ messager/__init__.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from tomllib import loads
 from messager.interfaces import Interface
 from messager.interfaces.terminal import Terminal
 from messager.messages import Message
@@ -9,4 +10,9 @@ __all__ = [
     'Message',
     "Terminal"
 ]
-__version__ = "0.1.0"
+__data = loads(
+    Path('pyproject.toml').read_text()
+)['tool']['poetry']
+__version__ = __data['version']
+__description__ = __data['description']
+__author__ = __data['authors']
